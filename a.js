ce435c91b7cbc43b22431ea7c40c355b5be24c87
Julian Ruiz <76981775+MJULIAN90@users.noreply.github.com>
2023-02-14 13:41:25 -0500

    Update a.js

--- a.js
+++ a.js
@@ -5,3 +5,20 @@ const chao = 'chao';
 const suma = (a, b) => {
     return a + b;
 }
+
+const hola = 'hola';
+
+const chao = 'chao';
+
+const suma = (a, b) => {
+    return a + b;
+}
+
+
+const hola = 'hola';
+
+const chao = 'chao';
+
+const suma = (a, b) => {
+    return a + b;
+}
